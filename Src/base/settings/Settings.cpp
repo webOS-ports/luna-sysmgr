50fce98951917f23fe13987e9adf405b05acb356
josh <jpalmerwatkins@me.com>
2012-10-26 21:05:56 +0100

    Card Window Manager: Implement Scaling
    * Increase card size if in phone ui
    * Layout modifications to accommodate scaling
    * Platform Conf: Increase card spacing
    
    Open-webOS-DCO-1.0-Signed-off-by: Josh Palmer <jpalmerwatkins@me.com>

--- Src/base/settings/Settings.cpp
+++ Src/base/settings/Settings.cpp
@@ -700,6 +700,7 @@ void Settings::postLoad()
 	positiveSpaceTopPadding *= uiScale;
 	positiveSpaceBottomPadding *= uiScale;
 	statusBarTitleMaxWidth *= uiScale;
+	gapBetweenCardGroups *= uiScale;
 }
 
 // Expands "1MB" --> 1048576, "2k" --> 2048, etc.
