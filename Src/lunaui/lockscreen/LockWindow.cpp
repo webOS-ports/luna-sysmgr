3a9cbabaa8462a9774b8f4280f0b83957a56bc10
josh <jpalmerwatkins@me.com>
2012-11-03 14:55:37 +0000

    Gesture Area: Scale Pixmap & Animation Tweaks
    * Lightbar is now scaled to virtualCoreNaviHeight/1.5
    * Animations now use the OutQuintic easing curve
    
    Open-webOS-DCO-1.0-Signed-off-by: Josh Palmer <jpalmerwatkins@me.com>

--- Src/lunaui/lockscreen/LockWindow.cpp
+++ Src/lunaui/lockscreen/LockWindow.cpp
@@ -539,7 +539,7 @@ void LockWindow::resize(int width, int height)
 	}
 
 	if(m_gestureArea) {
-		m_gestureArea->resize(width, height);
+		m_gestureArea->resize(width, Settings::LunaSettings()->virtualCoreNaviHeight);
 		m_gestureArea->update();
 	}
 
